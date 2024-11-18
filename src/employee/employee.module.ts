f7af3df4f786c12ea0171d76037cf319ac721cfd
Henrique Rodrigues <henrique.rodrigues@globalsys.com.br>
2024-11-18 11:43:33 -0300

    Remove employee controller

--- src/employee/employee.module.ts
+++ src/employee/employee.module.ts
@@ -1,10 +1,8 @@
 import { Module } from "@nestjs/common";
-import { EmployeeController } from "./employee.controller";
 import { EmployeeService } from "./employee.service";
 import { EmployeeResolver } from './employee.resolver';
 
 @Module({
-	controllers: [EmployeeController],
 	providers: [EmployeeService, EmployeeResolver],
 })
 export class EmployeeModule {}
