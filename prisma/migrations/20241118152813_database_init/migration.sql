061643abdd8f875ec6b0b97c53a2925707ca5db2
Henrique Klayton <henrique-klayton@users.noreply.github.com>
2024-11-18 14:52:47 -0300

    Squash into single migration

--- prisma/migrations/20241118152813_database_init/migration.sql
+++ prisma/migrations/20241118152813_database_init/migration.sql
@@ -4,7 +4,7 @@ CREATE TYPE "ScheduleType" AS ENUM ('SCHEDULE_6x1', 'SCHEDULE_5x1', 'SCHEDULE_12
 -- CreateTable
 CREATE TABLE "Employee" (
     "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
     "birthDate" DATE NOT NULL,
     "status" BOOLEAN NOT NULL DEFAULT true,
@@ -31,27 +31,10 @@ CREATE TABLE "Skill" (
     "id" SERIAL NOT NULL,
     "description" TEXT NOT NULL,
     "status" BOOLEAN NOT NULL DEFAULT true,
+    "employeeId" TEXT NOT NULL,
 
     CONSTRAINT "Skill_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "_EmployeeToSkill" (
-    "A" TEXT NOT NULL,
-    "B" INTEGER NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "_EmployeeToSkill_AB_unique" ON "_EmployeeToSkill"("A", "B");
-
--- CreateIndex
-CREATE INDEX "_EmployeeToSkill_B_index" ON "_EmployeeToSkill"("B");
-
 -- AddForeignKey
-ALTER TABLE "Employee" ADD CONSTRAINT "Employee_scheduleId_fkey" FOREIGN KEY ("scheduleId") REFERENCES "Schedule"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_EmployeeToSkill" ADD CONSTRAINT "_EmployeeToSkill_A_fkey" FOREIGN KEY ("A") REFERENCES "Employee"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "_EmployeeToSkill" ADD CONSTRAINT "_EmployeeToSkill_B_fkey" FOREIGN KEY ("B") REFERENCES "Skill"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Skill" ADD CONSTRAINT "Skill_employeeId_fkey" FOREIGN KEY ("employeeId") REFERENCES "Employee"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
