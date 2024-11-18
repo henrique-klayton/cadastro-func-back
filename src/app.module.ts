62a61aca2d104c9d457edec6bd3477ddddf49d2b
Henrique Rodrigues <henrique.rodrigues@globalsys.com.br>
2024-11-18 10:30:15 -0300

    Create database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmployeeModule } from './employee/employee.module';
+import { ScheduleModule } from './schedule/schedule.module';
+import { SkillModule } from './skill/skill.module';
 
 @Module({
-  imports: [],
+  imports: [EmployeeModule, ScheduleModule, SkillModule],
   controllers: [AppController],
   providers: [AppService],
 })
