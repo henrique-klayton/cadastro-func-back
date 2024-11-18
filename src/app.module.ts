3249886892ccee6c87de4f576c90be0732fea0d8
Henrique Rodrigues <henrique.rodrigues@globalsys.com.br>
2024-11-18 10:39:25 -0300

    Biome fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { EmployeeModule } from './employee/employee.module';
-import { ScheduleModule } from './schedule/schedule.module';
-import { SkillModule } from './skill/skill.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { EmployeeModule } from "./employee/employee.module";
+import { ScheduleModule } from "./schedule/schedule.module";
+import { SkillModule } from "./skill/skill.module";
 
 @Module({
-  imports: [EmployeeModule, ScheduleModule, SkillModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [EmployeeModule, ScheduleModule, SkillModule],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
