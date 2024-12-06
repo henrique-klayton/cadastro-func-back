106905c8a6a96d42e1960b8586ba7b1b737c231a
Henrique Rodrigues <henrique.rodrigues@globalsys.com.br>
2024-12-06 15:14:27 -0300

    Add debugging interceptor

---
+++ src/graphql/graphql.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { GraphqlInterceptor } from './graphql.interceptor';
+
+describe('GraphqlInterceptor', () => {
+  it('should be defined', () => {
+    expect(new GraphqlInterceptor()).toBeDefined();
+  });
+});
