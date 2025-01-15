2cded5a8b51991c2cbf9fcde5ec0e42edafa15dd
Henrique Klayton <henrique-klayton@users.noreply.github.com>
2025-01-15 09:15:23 -0300

    Update interceptor to show result and extra info

--- src/graphql/graphql.interceptor.ts
+++ src/graphql/graphql.interceptor.ts
@@ -12,17 +12,31 @@ import { Observable, tap } from "rxjs";
 export class GraphqlInterceptor implements NestInterceptor {
 	intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
 		return next.handle().pipe(
-			tap(() => {
+			tap((val: unknown) => {
+				// TODO Enable only in dev mode
 				if (context.getType<GqlContextType>() === "graphql") {
 					const ctx = GqlExecutionContext.create(context);
+					console.log(val, val?.constructor.name);
+					const res = ctx.getContext().res;
+					if (res != null) {
+						console.log(res);
+					}
 					const info: GraphQLResolveInfo = ctx.getInfo();
 					const fragments = info.fragments;
-					console.log(info.path);
-					console.log(info.rootValue);
-					console.log(info.parentType.name);
+					console.log(info.path); // Query/Mutation/Susbcription name and type
+					console.log(info.rootValue); // undefined?
+					console.log(info.parentType.name); // Is Query/Mutation/Susbcription
+					console.log(info.fieldName);
+					console.log(info.fieldNodes);
 					if (info.returnType instanceof GraphQLNonNull) {
 						const ofType = info.returnType.ofType;
-						if (!(ofType instanceof GraphQLList)) console.log(ofType.name);
+						if (!(ofType instanceof GraphQLList)) {
+							console.log(ofType.name);
+							if (ofType.name.startsWith("Paginated")) {
+								console.log(JSON.stringify(info.variableValues)); // Request Input
+								// console.log(ofType); // Output Values?
+							}
+						}
 					}
 				}
 			}),
